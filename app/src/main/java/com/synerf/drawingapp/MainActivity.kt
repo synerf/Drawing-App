51109abcd030e83d8ceb0d33502ea9e7e2d9f41f
Ashish Pimpre <ashishpimpre.connect@gmail.com>
2021-05-01 14:22:40 +0530

    added brush size selection functionality

--- app/src/main/java/com/synerf/drawingapp/MainActivity.kt
+++ app/src/main/java/com/synerf/drawingapp/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.synerf.drawingapp
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatImageButton
 import com.synerf.drawingapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +14,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.drawingView.setSizeForBrush(20.toFloat())
+        binding.ibBrush.setOnClickListener {
+            showBrushSizeChooserDialog()
+        }
+    }
+
+    private fun showBrushSizeChooserDialog() {
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Brush size: ")
+        val smallBtn = brushDialog.findViewById<AppCompatImageButton>(R.id.ibSmallBrush)
+        smallBtn.setOnClickListener {
+            binding.drawingView.setSizeForBrush(10.toFloat())
+            brushDialog.dismiss()
+        }
+        val mediumBtn = brushDialog.findViewById<AppCompatImageButton>(R.id.ibMediumBrush)
+        mediumBtn.setOnClickListener {
+            binding.drawingView.setSizeForBrush(20.toFloat())
+            brushDialog.dismiss()
+        }
+        val largeBtn = brushDialog.findViewById<AppCompatImageButton>(R.id.ibLargeBrush)
+        largeBtn.setOnClickListener {
+            binding.drawingView.setSizeForBrush(30.toFloat())
+            brushDialog.dismiss()
+        }
+        brushDialog.show()
     }
 }
